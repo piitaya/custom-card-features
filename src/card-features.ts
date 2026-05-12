4c92a2ffa5771164029e511f5820068e020e0ffe
Paul Bottein <paul.bottein@gmail.com>
2026-05-12 11:53:18 +0200

    Fix console log

--- src/card-features.ts
+++ src/card-features.ts
@@ -3,6 +3,6 @@ import { version } from "../package.json";
 import "./features/buttons";
 
 console.info(
-  `%c🍄 Custom Card Features DEV 🍄 - ${version}`,
+  `%c🍄 Custom Card Features 🍄 - ${version}`,
   "color: #ef5350; font-weight: 700;"
 );
